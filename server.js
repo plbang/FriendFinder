32e2e7ef9ceb39a4f8097f2dfdd14d75c277b18a
pbang <phillipbang@gmail.com>
2018-11-17 18:52:38 -0800

    heroku test

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
+// DEPENDENCIES
 var express = require("express");
 var path = require("path");
 
+// EXPRESS CONFIGURATION
 var app = express();
 var PORT = process.env.PORT || 3000;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// require("./app/routing/apiRoutes");
+// require("./app/routing/htmlRoutes");
 
 
 
-
+// LISTENER
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
