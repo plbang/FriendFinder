608f277efcdf54bde1b2b1ec3836b2f1c76e094b
pbang <phillipbang@gmail.com>
2018-11-18 18:39:17 -0800

    friend.js updated, exported

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+var friends = [
+  {
+    name: "Gerald",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "George",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Bambi",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+];
+
+// Must be exported for other files to use the data in the friends array
+module.exports = friends;
